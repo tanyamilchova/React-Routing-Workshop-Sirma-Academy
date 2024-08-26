314ca69bdde17b734fae07f675245e56c527ab7a
Tanya Milchova <margot11margot11@gmail.com>
2024-08-26 06:59:22 +0300

    Add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Header from "./components/Header"
 import {Routes, Route} from 'react-router-dom'
 import Home from "./components/Home"
 import Pricing from "./components/Pricing"
+import Login from "./components/Login"
+
+
+
+
 function App() {
-  
 
   return (
     <>
     <Header/>
     <Routes>
-        {/* <Route path='/' element={<Home/>} ></Route> */}
         <Route path='/' element={<Home/>} />
         <Route path='/pricing' element={<Pricing/>} />
-
+        <Route path='/login' element={<Login/>} />
     </Routes>
     </>
   )
